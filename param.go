b6a523c3782d5086d568f1fb238e21134a98e8c0
yujinhai <yujinhai12381@126.com>
2017-07-01 17:53:37 +0800

    var

---
+++ param.go
@@ -0,0 +1,17 @@
+package main
+
+var x,y int
+
+var (
+    a int
+    b bool
+)
+
+var c, d int = 1, 2
+var e, f = "123", "test"
+
+func main() {
+    g, h := "123", "func test"
+    println(x, y, a, b, c, d, e, f, g, h)
+}
+
---
+++ var.go
@@ -0,0 +1,12 @@
+package main
+
+var a = "若林源三"
+
+var b string = "足球小子"
+
+var c bool
+
+func main() {
+    println(a, b, c)
+}
+
