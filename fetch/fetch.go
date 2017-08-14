174c1650f450efd03608bd2a6fe238a139aaf334
yujinhai <yujinhai12381@126.com>
2017-08-14 19:49:26 +0800

    study

---
+++ fetch/fetch.go
@@ -0,0 +1,37 @@
+/*
+* File Name: fetch.go
+* Desc: 
+* Author:yujinhai
+* mail: yujinhai12381@126.com
+* Created Time: 一  8/14 19:38:33 2017
+*/
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+)
+
+func main() {
+    for _, url := range os.Args[1:] {
+        resp, err := http.Get(url)
+
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+            os.Exit(1)
+        }
+
+        b, err := ioutil.ReadAll(resp.Body)
+        resp.Body.Close()
+
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+            os.Exit(1)
+        }
+        
+        fmt.Printf("%s", b)
+    }
+}
+
