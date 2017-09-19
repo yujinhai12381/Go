ff635cb4b266799cc062767521730a86544627f0
yujinhai <yujinhai12381@126.com>
2017-09-19 11:26:55 +0800

    for

---
+++ loop/for.go
@@ -0,0 +1,17 @@
+/*
+* File Name: for.go
+* Desc: 
+* Author:yujinhai
+* mail: yujinhai12381@126.com
+* Created Time: 二  9/19 11:25:13 2017
+*/
+package main
+
+import "fmt"
+
+func main() {
+    for i := 0; i < 10 ; i++ {
+        fmt.Printf("%d\n", i)
+    }
+}
+
